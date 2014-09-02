3e33e3d5f19ffd09d902d10193a7aa2db33fe820
kirillne <kirillne35@gmail.com>
2014-09-02 11:47:46 +0300

    Class Shape was added

--- Shape/Shape.cs
+++ Shape/Shape.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Shape
 {
-    public class Shape
+    /// <summary>
+    /// Defines an geometric shape.
+    /// </summary>
+    public abstract class Shape
     {
-    }
+        /// <summary>
+        /// Gets or sets the color of this Shape.
+        /// </summary>
+        public Color Color { get; set; }
+
+        /// <summary>
+        /// Gets or sets the position of this Shape.
+        /// </summary>
+        public Point Position { get; set; }
+
+        /// <summary>
+        /// Draw this Shape on graphics.
+        /// </summary>
+        /// <param name="graphics">Surfase for drawing.</param>
+        public abstract void Draw(Graphics graphics);
+
+   }
 }
