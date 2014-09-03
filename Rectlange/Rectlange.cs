dbb87fa5abb5632c792b5693fa644cb59e7e7d51
kirillne <kirillne35@gmail.com>
2014-09-03 22:22:40 +0300

    Class Rectlange been aded

---
+++ Rectlange/Rectlange.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1.Shapes
+{
+    /// <summary>
+    /// Defines an rectlange.
+    /// </summary>
+    public class Rectlange : RectlangeBaseShape
+    {
+         /// <summary>
+        /// Initializes a new instance of the Rectlange class whith black color, (0;0) position and zero size.
+        /// </summary>
+        public Rectlange() : base()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the Rectlange class with the specified color and position and zero size.
+        /// </summary>
+        public Rectlange(Color color, Point position) : base(color, position)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the Rectlange class with the specified color, position, size.
+        /// </summary>
+        public Rectlange(Color color, Point position, int height, int width)
+            : base(color, position, height, width)
+        {
+        }
+
+        /// <summary>
+        /// Draw this Rectlange on graphics.
+        /// </summary>
+        /// <param name="graphics">Surfase for drawing.</param>
+        public override void Draw(Graphics graphics)
+        {
+            graphics.DrawRectangle(new Pen(Color),Position.X, Position.Y, Position.X + Width, Position.Y + Height );
+        }
+    }
+}
