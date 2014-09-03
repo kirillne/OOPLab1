c3ebbec564eb5339c43eb324b02a68c9da3ea965
kirillne <kirillne35@gmail.com>
2014-09-03 22:07:06 +0300

    RectlangeBaseShape been aded

--- Line/Line.cs
+++ Line/Line.cs
@@ -10,45 +10,14 @@ namespace Lab1.Shapes
     /// <summary>
     /// Defines an line.
     /// </summary>
-    public class Line: Shape
+    public class Line: RectlangeBaseShape
     {
-        private int height;
-
-        /// <summary>
-        /// Gets or sets the vertical difference between start and end points.
-        /// </summary>
-        public int Height
-        {
-            get { return height; }
-            set
-            {
-                if(height < 0) throw  new ArgumentOutOfRangeException("value", "Line can't have nagative height");
-                height = value;
-            }
-        }
-
-        private int width;
-
-        /// <summary>
-        /// Gets or sets the horisontal difference between start and end points.
-        /// </summary>
-        public int Width
-        {
-            get { return width; }
-            set
-            {
-                if (width < 0) throw new ArgumentOutOfRangeException("value", "Line can't have nagative width");
-                width = value;
-            }
-        }
 
         /// <summary>
         /// Initializes a new instance of the Line class whith black color, (0;0) position and zero size.
         /// </summary>
-        public Line():base()
+        public Line() : base()
         {
-            height = 0;
-            width = 0;
         }
 
         /// <summary>
@@ -56,18 +25,14 @@ namespace Lab1.Shapes
         /// </summary>
         public Line(Color color, Point position) : base(color, position)
         {
-            height = 0;
-            width = 0;
         }
 
         /// <summary>
         /// Initializes a new instance of the Line class with the specified color, position, size.
         /// </summary>
         public Line(Color color, Point position, int height, int width)
-            : base(color, position)
+            : base(color, position, height, width
         {
-            this.height = height;
-            this.width = width;
         }
 
         /// <summary>
@@ -76,7 +41,7 @@ namespace Lab1.Shapes
         /// <param name="graphics">Surfase for drawing.</param>
         public override void Draw(Graphics graphics)
         {
-            graphics.DrawLine(new Pen(Color),Position, Position + new Size(width, height) );
+            graphics.DrawLine(new Pen(Color),Position, Position + new Size(Width, Height) );
         }
     }
 }
---
+++ Shape/RectlangeBaseShape.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    /// <summary>
+    /// Defines an shape? wich can drawing in rectlange.
+    /// </summary>
+    public abstract class RectlangeBaseShape : Shape
+    {
+        private int height;
+
+        /// <summary>
+        /// Gets or sets the vertical difference between start and end points.
+        /// </summary>
+        public int Height
+        {
+            get { return height; }
+            set
+            {
+                if(height < 0) throw  new ArgumentOutOfRangeException("value", "Line can't have nagative height");
+                height = value;
+            }
+        }
+
+        private int width;
+
+        /// <summary>
+        /// Gets or sets the horisontal difference between start and end points.
+        /// </summary>
+        public int Width
+        {
+            get { return width; }
+            set
+            {
+                if (width < 0) throw new ArgumentOutOfRangeException("value", "Line can't have nagative width");
+                width = value;
+            }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the Shape class whith black color, (0;0) position and zero size.
+        /// </summary>
+        protected RectlangeBaseShape():base()
+        {
+            height = 0;
+            width = 0;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the Shape class with the specified color and position and zero size.
+        /// </summary>
+        protected RectlangeBaseShape(Color color, Point position) : base(color, position)
+        {
+            height = 0;
+            width = 0;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the Line class with the specified color, position, size.
+        /// </summary>
+        protected RectlangeBaseShape(Color color, Point position, int height, int width)
+            : base(color, position)
+        {
+            this.height = height;
+            this.width = width;
+        }
+
+    }
+}
