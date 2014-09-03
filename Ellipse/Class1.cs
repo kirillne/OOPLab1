f9d1e0c8af80582d7c2087f4183c8e9214041614
kirillne <kirillne35@gmail.com>
2014-09-03 22:27:44 +0300

    Class Ellipse added

---
+++ Ellipse/Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1.Shape
+{
+    /// <summary>
+    /// Defines an ellipse.
+    /// </summary>
+    public class Ellipse : RectlangeBaseShape
+    {
+        /// <summary>
+        /// Initializes a new instance of the Ellipse class whith black color, (0;0) position and zero size.
+        /// </summary>
+        public Ellipse() : base()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the Ellipse class with the specified color and position and zero size.
+        /// </summary>
+        public Ellipse(Color color, Point position) : base(color, position)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the Ellipse class with the specified color, position, size.
+        /// </summary>
+        public Ellipse(Color color, Point position, int height, int width)
+            : base(color, position, height, width)
+        {
+        }
+
+        /// <summary>
+        /// Draw this Ellipse on graphics.
+        /// </summary>
+        /// <param name="graphics">Surfase for drawing.</param>
+        public override void Draw(Graphics graphics)
+        {
+            graphics.DrawEllipse(new Pen(Color),Position.X, Position.Y, Position.X + Width, Position.Y + Height );
+        }
+    }
+}
