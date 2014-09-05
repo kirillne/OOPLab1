fc15b87871879ce66125bbef476b2a32d33500a7
kirillne <kirillne35@gmail.com>
2014-09-05 20:55:38 +0300

    Class ShapeList been writed

--- ShapeList/ShapeList.cs
+++ ShapeList/ShapeList.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,12 @@ namespace Lab1
 {
     public class ShapeList : List<Shape>
     {
+        public void DrawAll(Graphics graphics)
+        {
+            foreach (var shape in this)
+            {
+                shape.Draw(graphics);
+            }
+        }
     }
 }
